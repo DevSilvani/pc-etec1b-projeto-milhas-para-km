0dcc3ff8e4ab5443af01bf74b4779bfb84a7ee32
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-23 17:57:21 -0300

    c# pronto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pc_etec1b_projeto_milhas_para_km
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double milha = 0.0, km = 0.0;
+
+            Console.Write("digite quantas milhas quer converter: ");
+            milha = Convert.ToDouble(Console.ReadLine());
+
+            km = milha * 1.60934;
+
+            Console.WriteLine(milha+" Milhas são "+km+" KM");
+            Console.ReadKey();
+        }
+    }
+}
